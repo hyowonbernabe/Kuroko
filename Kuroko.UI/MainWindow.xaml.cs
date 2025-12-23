a74a6fab9f010bec941b6bf09e647497df64a627
hyowonbernabe <hyo.arzil.bernabe@gmail.com>
2025-12-23 14:53:53 +0800

    Setup window to not appear in screen capture

--- Kuroko.UI/MainWindow.xaml.cs
+++ Kuroko.UI/MainWindow.xaml.cs
@@ -1,24 +1,37 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
-namespace Kuroko.UI
+namespace Kuroko.UI;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private const uint WDA_NONE = 0x00000000;
+    private const uint WDA_MONITOR = 0x00000001;
+    private const uint WDA_EXCLUDEFROMCAPTURE = 0x00000011;
+
+    [DllImport("user32.dll")]
+    public static extern uint SetWindowDisplayAffinity(IntPtr hWnd, uint dwAffinity);
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        EnableStealthMode();
+    }
+
+    private void EnableStealthMode()
+    {
+        var helper = new WindowInteropHelper(this);
+        SetWindowDisplayAffinity(helper.Handle, WDA_EXCLUDEFROMCAPTURE);
+    }
+
+    private void ExitButton_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
     }
 }
