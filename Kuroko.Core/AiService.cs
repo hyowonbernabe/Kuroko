9daca368ef501867915a2a0d73c64fd29c3f1661
hyowonbernabe <hyo.arzil.bernabe@gmail.com>
2025-12-23 16:13:56 +0800

    Implement HotKey for running LLM

---
+++ Kuroko.Core/AiService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kuroko.Core
+{
+    internal class AiService
+    {
+    }
+}
---
+++ Kuroko.UI/HotkeyService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kuroko.UI
+{
+    class HotkeyService
+    {
+    }
+}
